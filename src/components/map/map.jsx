f20750c3326e1459629e5d66a7bc29addec30901
Zarif <zarifbeta@gmail.com>
2020-07-02 17:34:31 +0600

    patterns-2

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -5,42 +5,46 @@ import leaflet from "leaflet";
 class Map extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.sectiontRef = React.createRef();
   }
 
   componentDidMount() {
-    const city = [52.38333, 4.9];
+    if (this.sectiontRef.current) {
+      const city = [52.38333, 4.9];
 
-    const icon = leaflet.icon({
-      iconUrl: `img/pin.svg`,
-      iconSize: [30, 30]
-    });
+      const icon = leaflet.icon({
+        iconUrl: `img/pin.svg`,
+        iconSize: [30, 30]
+      });
 
-    const zoom = 12;
-    const map = leaflet.map(`map`, {
-      center: city,
-      zoom,
-      zoomControl: false,
-      marker: true
-    });
+      const zoom = 12;
+      const map = leaflet.map(this.sectiontRef.current, {
+        center: city,
+        zoom,
+        zoomControl: false,
+        marker: true
+      });
 
-    leaflet
-    .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
-    })
-    .addTo(map);
-
-    map.setView(city, zoom);
-
-    for (let i = 0; i < this.props.offers.length; i++) {
       leaflet
-      .marker(this.props.offers[i].coordinates, {icon})
+      .tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+        attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+      })
       .addTo(map);
+
+      map.setView(city, zoom);
+
+      for (let i = 0; i < this.props.offers.length; i++) {
+        leaflet
+        .marker(this.props.offers[i].coordinates, {icon})
+        .addTo(map);
+      }
     }
   }
 
   render() {
     return (
-      <section id="map" className="cities__map map"></section>
+      <section ref={this.sectiontRef} id="map" className="cities__map map"></section>
     );
   }
 }
