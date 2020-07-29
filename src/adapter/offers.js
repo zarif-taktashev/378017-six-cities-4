fa80f8d1a2f29340cfae89531963a1a8f33c9e3b
Zarif <zarifbeta@gmail.com>
2020-07-29 22:44:52 +0600

    ajax

---
+++ src/adapter/offers.js
@@ -0,0 +1,38 @@
+const offerAdapter = (offer) => {
+  return {
+    bedrooms: offer.bedrooms,
+    city: {
+      location: {
+        latitude: offer.city.location.latitude,
+        longitude: offer.city.location.longitude,
+        zoom: offer.city.location.zoom
+      },
+      name: offer.city.name
+    },
+    description: offer.description,
+    goods: offer.goods,
+    host: {
+      avatarUrl: offer.host.avatar_url,
+      id: offer.host.id,
+      isPro: offer.host.is_pro,
+      name: offer.host.name
+    },
+    id: offer.id,
+    images: offer.images,
+    isFavorite: offer.is_favorite,
+    isPremium: offer.is_premium,
+    location: {
+      latitude: offer.location.latitude,
+      longitude: offer.location.longitude,
+      zoom: offer.location.zoom
+    },
+    maxAdults: offer.max_adults,
+    previewImage: offer.preview_image,
+    price: offer.price,
+    rating: offer.rating,
+    title: offer.title,
+    type: offer.type,
+  };
+};
+
+export {offerAdapter};
